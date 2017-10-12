e6224054416b9011148306fbd2ba1bb0f71101a9
Barberdad <sebastian.rasimus@gmail.com>
2017-10-12 12:45:21 +0200

    Fixed path to msh files

--- worker/runAirfoil.sh
+++ worker/runAirfoil.sh
@@ -5,6 +5,7 @@ N_NODES=$3;
 MESHNAME=r${N_LEVELS}a${ANGLE}n${N_NODES};
 cd /home/fenics/local/ACC-1/murtazo/cloudnaca/;
 ./runme.sh $ANGLE $ANGLE $N_LEVELS $N_NODES;
+cd /home/fenics/local/ACC-1/murtazo/cloudnaca/msh/;
 dolfin-convert ${MESHNAME}.msh ${MESHNAME}.xml;
 cd /home/fenics/local/ACC-1/murtazo/navier_stokes_solver/
 ./airfoil 10 0.0001 10. 0.001 ./murtazo/cloudnaca/msh/${MESHNAME}.xml
