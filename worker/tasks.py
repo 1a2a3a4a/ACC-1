e9aea728f369951acd0230069894850f7f9b1ccb
Barberdad <sebastian.rasimus@gmail.com>
2017-10-12 15:05:59 +0200

    Fixed typo

--- worker/tasks.py
+++ worker/tasks.py
@@ -12,7 +12,7 @@ def add(x, y):
 def airfoil(start_angle, stop_angle, n_angles, n_levels, n_nodes):
         result = ""
         resultFile = '../murtazo/navier_stokes_solver/results/drag_ligt.m'
-        subprocess.call('sh runAirfoil.sh {0} {1} {2} {3} {4} {5}'.format(start_angle, stop_angle, n_angles, n_nodes, n_levels))
+        subprocess.call('sh runAirfoil.sh {0} {1} {2} {3} {4}'.format(start_angle, stop_angle, n_angles, n_nodes, n_levels))
         with open(resultFile, 'r') as f:
                 result = f.read()
         subprocess.call('sh cleanWorker.sh')
