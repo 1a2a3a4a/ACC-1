4640d574bff0beea7876fc65b30199587312bff1
Barberdad <sebastian.rasimus@gmail.com>
2017-10-12 14:54:30 +0200

    Correct usage of open with

--- worker/tasks.py
+++ worker/tasks.py
@@ -9,8 +9,10 @@ def add(x, y):
         return x + y
     
 def airfoil(start_angle, stop_angle, n_angles, n_levels, n_nodes):
+        result = ""
         resultFile = '../murtazo/navier_stokes_solver/results/drag_ligt.m'
         subprocess.call('sh runAirfoil.sh {0} {1} {2} {3} {4} {5}'.format(start_angle, stop_angle, n_angles, n_nodes, n_levels))
-        with open(resultFile, 'r'):
-                return results.read()
-        
+        with open(resultFile, 'r') as f:
+                result = f.read()
+        subprocess.call('sh cleanWorker.sh')
+        return result
