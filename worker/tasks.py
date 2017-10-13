8034b700513894e715ca8c1a0221c4c263d0a3cd
Barberdad <sebastian.rasimus@gmail.com>
2017-10-13 15:36:27 +0200

    Shell-scripts called by tasks now prints the commands

--- worker/tasks.py
+++ worker/tasks.py
@@ -8,12 +8,12 @@ app.config_from_object('celeryconfig')
 @app.task
 def add(x, y):
         return x + y
-    
-def airfoil(start_angle, stop_angle, n_angles, n_levels, n_nodes):
+@app.task    
+def airfoil(start_angle, stop_angle, n_angles, n_nodes, n_levels):
         result = ""
         resultFile = '../murtazo/navier_stokes_solver/results/drag_ligt.m'
-        subprocess.call('sh runAirfoil.sh {0} {1} {2} {3} {4}'.format(start_angle, stop_angle, n_angles, n_nodes, n_levels), shell=True)
+        subprocess.call('sh -x runAirfoil.sh {0} {1} {2} {3} {4}'.format(start_angle, stop_angle, n_angles, n_nodes, n_levels), shell=True)
         with open(resultFile, 'r') as f:
                 result = f.read()
-        subprocess.call('sh cleanWorker.sh')
+        subprocess.call('sh -x cleanWorker.sh', shell=True)
         return result
